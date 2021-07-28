d6e58cc866cb9e641ebc4d9ac811920b156961d9
A-Konig <44848614+A-Konig@users.noreply.github.com>
2021-07-28 15:17:56 +0200

    Clustering Interface

---
+++ MeshAnimation/Clustering/IClustering.cs
@@ -0,0 +1,17 @@
+﻿using OpenTkRenderer.Rendering.Meshes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeshAnimation.Clustering
+{
+    interface IClustering
+    {
+        List<int[]> BoneClusters { get; }
+        MeshLoader[] Animation { get; set; }
+
+        bool Cluster(MeshLoader restPose);
+    }
+}
---
+++ MeshAnimation/Clustering/KMeans.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTkRenderer.Rendering.Meshes;
+
+namespace MeshAnimation.Clustering
+{
+    class KMeans : IClustering
+    {
+        public int BoneCount;
+
+        List<int[]> boneClusters;
+        public List<int[]> BoneClusters { get => boneClusters; }
+
+        MeshLoader[] animation;
+        public MeshLoader[] Animation { get => animation; set => animation = value; }
+
+        public bool Cluster(MeshLoader restPose)
+        {
+            if (BoneCount <= 0)
+                return false;
+
+            // call clustering library
+
+            throw new NotImplementedException();
+        }
+    }
+}
